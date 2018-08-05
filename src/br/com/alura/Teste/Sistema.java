95a7e1a59ba2170cb4a06aa3ea500124c34bd18f
c129427 <36902864+c129427@users.noreply.github.com>
2018-08-05 08:22:39 -0300

    Capito1Video3
    Finalizado o Video 3 do  Capitulo 1, sobre como pegar os campos nome e preco no xml

---
+++ src/br/com/alura/Teste/Sistema.java
@@ -0,0 +1,37 @@
+package br.com.alura.Teste;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import br.com.alura.Model.Produto;
+
+
+public class Sistema {
+	
+
+	public static void main(String[] args) throws Exception {
+		
+		DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = fabrica.newDocumentBuilder();
+		Document document = builder.parse("src/vendas.xml");
+		
+		NodeList produtos = document.getElementsByTagName("produto");
+		
+		for(int i=0;i < produtos.getLength();i++) {
+			Element produto = (Element) produtos.item(i);
+			String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+			Double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
+			Produto prod = new Produto(nome, preco);
+			
+			System.out.println(prod);
+			
+			
+		}
+
+	}
+
+}
